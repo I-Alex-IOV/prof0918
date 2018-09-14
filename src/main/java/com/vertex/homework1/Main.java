5e356d53087bfbd18d2fae3bad4af1a3870736d3
backumich <backumich@gmail.com>
2018-09-14 19:06:08 +0300

    Initial commit

---
+++ src/main/java/com/vertex/homework1/Main.java
@@ -0,0 +1,9 @@
+package com.vertex.homework1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, my first homework");
+        System.out.println("Markers should be here");
+    }
+}
---
+++ src/test/java/com/vertex/ExampleTest.java
@@ -0,0 +1,4 @@
+package com.vertex;
+
+public class ExampleTest {
+}
