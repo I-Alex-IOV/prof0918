b673154831ed0642ba6532b3e2f1861329c675e8
Aleksandr <cameramnet1@gmail.com>
2018-09-23 12:17:15 +0300

    homework2

--- src/main/java/com/vertex/homework2/p/Artist.java
+++ src/main/java/com/vertex/homework2/p/Artist.java
@@ -11,43 +11,58 @@ import com.vertex.homework2.figure.Point;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 
 public class Artist implements ArtisrInterface {
     Point point = new Point(0,0);
     private int quantity;
-    private Queue<Figure > figuresQueue = new LinkedList<>();
+    final BlockingQueue<Figure> queue = new ArrayBlockingQueue<Figure>(100);
 
-
-    public void arrayFilling() {
-        for (int i = 0; i < 25; i++) {
-            figuresQueue.add(FiguresSelector.getInstance().figures(TypeOfFigure.CIRCLE));
-            figuresQueue.add(FiguresSelector.getInstance().figures(TypeOfFigure.OVAL));
-            figuresQueue.add(FiguresSelector.getInstance().figures(TypeOfFigure.SQUARE));
-            figuresQueue.add(FiguresSelector.getInstance().figures(TypeOfFigure.RECTANGULAR));
-        }
-    }
     @Override
     public void draw() {
         Scanner in = new Scanner(System.in);
         System.out.println("How many pieces to draw? ");
         quantity = in.nextInt();
         in.close();
-        Figure figure ;
-        arrayFilling();
-        if (figuresQueue != null) {
-            for (int i = 0; i < quantity; i++) {
-                figure = figuresQueue.poll();
-                System.out.println(i);
-                figure.showFigure(point);
-                if (figure instanceof Oval) {
-                    ((OvalDecorator) figure).moShowFigure();
-                    if (figure instanceof Oval) {
-                        ((RectagularDecorator) figure).moShowFigure();
+
+        new Thread() {
+            public void run() {
+                for (int i = 0; i < quantity; i++) {
+                    try {
+                        queue.put((FiguresSelector.getInstance().figures(TypeOfFigure.CIRCLE)));
+                        queue.put((FiguresSelector.getInstance().figures(TypeOfFigure.OVAL)));
+                        queue.put((FiguresSelector.getInstance().figures(TypeOfFigure.SQUARE)));
+                        queue.put((FiguresSelector.getInstance().figures(TypeOfFigure.RECTANGULAR)));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-        }
+        }.start();
+        new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(2_000);
+                    Figure figure;
+                    for (int i = 0; i < quantity; i++) {
+                        figure = queue.take();
+                        System.out.println(i);
+                        figure.showFigure(point);
+                        if (figure instanceof Oval) {
+                            ((OvalDecorator) figure).moShowFigure();
+                            if (figure instanceof Oval) {
+                                ((RectagularDecorator) figure).moShowFigure();
+                            }
+                        }
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
 
