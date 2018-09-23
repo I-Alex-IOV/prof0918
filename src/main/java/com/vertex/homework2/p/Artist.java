d422cb7e360b575b5b88a67d373c3b85bb37936c
Aleksandr <cameramnet1@gmail.com>
2018-09-23 12:51:26 +0300

    homework2

--- src/main/java/com/vertex/homework2/p/Artist.java
+++ src/main/java/com/vertex/homework2/p/Artist.java
@@ -10,6 +10,7 @@ import com.vertex.homework2.figure.Point;
 import com.vertex.homework2.figure.Rectangular;
 
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -64,6 +65,32 @@ public class Artist implements ArtisrInterface {
                 }
             }
         }.start();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Artist{");
+        sb.append("point=").append(point);
+        sb.append(", quantity=").append(quantity);
+        sb.append(", queue=").append(queue);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Artist)) return false;
+        Artist artist = (Artist) o;
+        return quantity == artist.quantity &&
+                Objects.equals(point, artist.point) &&
+                Objects.equals(queue, artist.queue);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(point, quantity, queue);
     }
 }
 
