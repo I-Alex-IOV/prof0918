4820fe39aebb7478db7f9e73834b061ba5af5f52
backumich <backumich@gmail.com>
2018-09-24 22:30:59 +0300

    comments to Homework1

--- src/main/java/com/vertex/homework1/Main.java
+++ src/main/java/com/vertex/homework1/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Teacher teacher = new Teacher();
+        // TODO: 24.09.2018 since wrong naming now it is not clear why do we are getting Market two times in a row 
         teacher.writeText(teacher.getMarker().getMarker(MarkerTypes.RED), "Красный текст");
         teacher.writeText(teacher.getMarker().getMarker(MarkerTypes.BLACK), "Черный текст");
         teacher.writeText(teacher.getMarker().getMarker(MarkerTypes.GREEN), "Зеленый текст");
--- src/main/java/com/vertex/homework1/Teacher.java
+++ src/main/java/com/vertex/homework1/Teacher.java
@@ -5,12 +5,15 @@ import com.vertex.homework1.markerFactory.MarkerFactory;
 
 class Teacher {
 
+    // TODO: 24.09.2018 not a good name for a MarketFactory
+    // TODO: 24.09.2018 does a teacher have his own factory? Strange, I don't =)
     private MarkerFactory marker;
 
     Teacher(){
         marker = new MarkerFactory();
     }
 
+    // TODO: 24.09.2018 getMarker should return Marker
     MarkerFactory getMarker() {
         return marker;
     }
